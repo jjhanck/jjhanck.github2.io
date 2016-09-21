4a9c393def972ac71f07705aea1ea2044d6382f7
jjhanck <jjhanck@yahoo.com>
2016-09-21 11:09:53 -0500

    Add files via upload

---
+++ js/skill.js
@@ -0,0 +1,32 @@
+var myBarChart = new Chart(ctx, {
+    type: 'horizontalBar',
+    data: data,
+    options: options
+});
+
+var data = {
+    labels: ["Business Intelligence", "SQL", "Statistics", "Python", "Team Management", "Platfomr Management", "Communication"],
+    datasets: [
+        {
+            label: "My First dataset",
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1,
+            data: [100, 80, 60, 40, 100, 100, 100],
+        }
+    ]
+};
