8f13d16fe03fbe494d9d2f440b4d7063d7a1982d
jjhanck <jjhanck@yahoo.com>
2016-09-21 11:23:41 -0500

    Add files via upload

--- js/skill.js
+++ js/skill.js
@@ -1,32 +1,39 @@
-var myBarChart = new Chart(ctx, {
-    type: 'horizontalBar',
+new Chart(ctx, {
     data: data,
+    type: 'polarArea',
     options: options
 });
 
+
 var data = {
-    labels: ["Business Intelligence", "SQL", "Statistics", "Python", "Team Management", "Platfomr Management", "Communication"],
-    datasets: [
-        {
-            label: "My First dataset",
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1,
-            data: [100, 80, 60, 40, 100, 100, 100],
-        }
+    datasets: [{
+        data: [
+            20,
+            16,
+            10,
+            7,
+            18,
+			18,
+			20
+        ],
+        backgroundColor: [
+            "#FF6384",
+            "#4BC0C0",
+            "#FFCE56",
+            "#E7E9ED",
+            "#36A2EB",
+			"#E04321",
+			"##C8C7CB"
+        ],
+        label: 'My dataset' // for legend
+    }],
+    labels: [
+        "Business Intelligence",
+        "SQL",
+        "Statistics",
+        "Python",
+        "Team Management"
+		"Platform Management"
+		"Communication"
     ]
 };
