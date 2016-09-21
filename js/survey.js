bb96421c4bc05cc9f9ccebde5ed02d1b19dd0b88
jjhanck <jjhanck@yahoo.com>
2016-09-21 18:51:06 -0500

    Add files via upload

--- js/survey.js
+++ js/survey.js
@@ -5,7 +5,7 @@ var data2 = {
     labels: ["Customers knew who the team was", "Customers knew support member by name", "Customers were satisfied with platform support", "Customers felt they understood platform direction", "% would reccomend team", "Technologies fit needs", "I like James"],
     datasets: [
         {
-            label: "Results of pre- James Customer satisfation survey",
+            label: "Survey Conducted Before James' Strategic Intiatives",
             backgroundColor: "rgba(179,181,198,0.2)",
             borderColor: "rgba(179,181,198,1)",
             pointBackgroundColor: "rgba(179,181,198,1)",
@@ -15,7 +15,7 @@ var data2 = {
             data: [10, 5, 25, 6, 45, 50, 0]
         },
         {
-            label: "James' Ideal Environement",
+            label: "Survey Conducted After James' Strategic Intiatives",
             backgroundColor: "rgba(255,99,132,0.2)",
             borderColor: "rgba(255,99,132,1)",
             pointBackgroundColor: "rgba(255,99,132,1)",
